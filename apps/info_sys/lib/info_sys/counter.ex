6286c0c8abe5f1791348b3785333c439598fde89
Samuel Monroe <spat.monroe@gmail.com>
2022-06-07 07:07:25 +0200

    Counter is a supervised GenServer

--- apps/info_sys/lib/info_sys/counter.ex
+++ apps/info_sys/lib/info_sys/counter.ex
@@ -1,36 +1,41 @@
 defmodule InfoSys.Counter do
-  def inc(pid), do: send(pid, :inc)
+  use GenServer
+  def inc(pid), do: GenServer.cast(pid, :inc)
 
-  def dec(pid), do: send(pid, :dec)
+  def dec(pid), do: GenServer.cast(pid, :dec)
 
-  def val(pid, timeout \\ 5000) do
-    ref = make_ref()
-    send(pid, {:val, self(), ref})
-
-    receive do
-      {^ref, value} -> value
-    after
-      timeout -> exit(:timeout)
-    end
-  end
+  def val(pid), do: GenServer.call(pid, :val)
 
   def start_link(initial_val) do
-    {:ok, spawn_link(fn -> listen(initial_val) end)}
+    GenServer.start_link(__MODULE__, initial_val)
   end
 
-  defp listen(val) do
-    receive do
-      :inc ->
-        listen(val + 1)
+  # Implementation
+  @impl true
+  def init(initial_val) do
+    Process.send_after(self(), :tick, 1000)
+    {:ok, initial_val}
+  end
 
-      :dec ->
-        listen(val - 1)
+  @impl true
+  def handle_cast(:inc, val) do
+    {:noreply, val+1}
+  end
 
-      {:val, sender, ref} ->
-        send(sender, {ref, val})
+  @impl true
+  def handle_cast(:dec, val) do
+    {:noreply, val-1}
+  end
 
-        listen(val)
-    end
+  @impl true
+  def handle_call(:val, _from, val) do
+    {:reply, val, val}
+  end
 
+  @impl true
+  def handle_info(:tick, val) do
+    IO.puts("tick #{val}")
+    Process.send_after(self(), :tick, 1000)
+    {:noreply, val-1}
   end
 end
