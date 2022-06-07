e1c01d153578c5270b0c30abe392d739b27d9a74
Samuel Monroe <spat.monroe@gmail.com>
2022-06-07 08:21:33 +0200

    InfoSys querying interface

--- apps/info_sys/lib/info_sys.ex
+++ apps/info_sys/lib/info_sys.ex
@@ -3,16 +3,28 @@ defmodule InfoSys do
   Documentation for `InfoSys`.
   """
 
-  @doc """
-  Hello world.
+  @backends [InfoSys.Wolfram]
 
-  ## Examples
+  defmodule Result do
+    defstruct score: 0, text: nil, backend: nil
+  end
 
-      iex> InfoSys.hello()
-      :world
+  def compute(query, opts \\ []) do
+    opts = Keyword.put_new(opts, :limit, 10)
 
-  """
-  def hello do
-    :world
+    backends = opts[:backends] || @backends
+
+    backends
+    |> Enum.map(&async_query(&1, query, opts))
+  end
+
+  defp async_query(backend, query, opts) do
+    Task.Supervisor.async_nolink(
+      InfoSys.TaskSupervisor,
+      backend,
+      :compute,
+      [query, opts],
+      shutdown: :brutal_kill
+    )
   end
 end
